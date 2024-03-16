20da337ceb75b36b2f2170d07057cbf0d7933261
Davi Demarqui <davicoding@protonmail.com>
2024-03-16 18:26:10 -0300

    First Commit

---
+++ contracts/WGovernance.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+import {IWGovernance} from "contracts/interfaces/IWGovernance.sol";
+
+contract WGovernance is IWGovernance {
+
+}
