55928f8a7ccd9e338c1cf794eca9d564f856b83d
Dave <davicoding@protonmail.com>
2024-03-18 01:58:42 -0300

    Fixing syntax errors

--- contracts/types/Proposal.sol
+++ contracts/types/Proposal.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
 
+enum ProposaType {
+    MemberRem,
+    FeeUpdate,
+    StkUpdate,
+    CategUpdate,
+    SkillsUpdate
+}
+
 struct Proposal {
-    address indexed creator;
+    address creator;
     address[] voters; // Also used to count votes;
-    enum ProposaType {
-        MemberRem,
-        FeeUpdate,
-        StkUpdate,
-        CategUpdate,
-        SkillsUpdate
-    }
+    ProposaType proposaType;
     uint256 votingPeriod;
 }
