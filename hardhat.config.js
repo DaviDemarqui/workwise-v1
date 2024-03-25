b7102f2a8643f4726dac5a4c17908a704d449436
Dave <davicoding@protonmail.com>
2024-03-25 01:34:27 -0300

    Sepolia Config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,35 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config(); 
+
+const pvKey = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.24",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/sc2zmD1h0Va_v44idX0A4HPhxpEKOcka",
+      accounts: [pvKey]
+    }
+  },
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
 };
